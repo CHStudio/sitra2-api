8489da65a36075621229b0911f1dbb702de8f936
Stéphane HULARD <s.hulard@chstudio.fr>
2014-06-12 11:59:22 +0200

    Création du prototype d'affichage des partenaires techniques Sitra2

--- public/index.php
+++ public/index.php
@@ -54,9 +54,9 @@
 			<div id="filters" class="panel-collapse collapse in">
 				<div class="panel-body">
 					<ul class="nav nav-pills nav-stacked">
-						<li><a href="#">Tous les partenaires</a></li>
-						<li><a href="#">Profile</a></li>
-				  	<li><a href="#">Messages</a></li>
+						<li><a href="#" data-criteria="criteresQuery=critereInterne:CritereInterne_5219">Membre Formateur</a></li>
+						<li><a href="#" data-criteria="criteresQuery=critereInterne:CritereInterne_5218">Membre Prestataire Technique Editeur</a></li>
+				  	<li><a href="#" data-criteria="criteresQuery=critereInterne:CritereInterne_3872">Membre Prestataire Technique</a></li>
 					</ul>
 				</div>
 			</div>
@@ -74,8 +74,7 @@
 					zoom: 8,
 					center: new google.maps.LatLng(45.750000, 4.850000)
 				};
-				map = new google.maps.Map(document.getElementById('map'),
-						mapOptions);
+				map = new google.maps.Map(document.getElementById('map'), mapOptions);
 			}
 
 			google.maps.event.addDomListener(window, 'load', initialize);
@@ -85,6 +84,73 @@
 			});
 			$('#filters').on('shown.bs.collapse', function () {
 			  $(this).parent().find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+			});
+
+			var defaultQuery = '&responseFields=["id","nom","informations","localisation"]';
+
+			$('#filters a').click( function() {
+				if( this._markers !== undefined ) {
+					$(this._markers).each( function(index, item) {
+						if( this._visible === true ) {
+							item.setMap(null);
+						} else {
+							item.setMap(map);
+						}
+					}.bind(this));
+					$(this).parent().toggleClass('active');
+					this._visible = !this._visible;
+				} else {
+					$(this).parent().addClass('active');
+					$.ajax(
+						window.location.href.replace('index.php','').replace('#', '')+'ajax.php?'+$(this).attr('data-criteria')+defaultQuery,
+						{
+							dataType: 'json',
+							success: function(response) {
+								$(this).html($(this).html()+" ("+response.length+")");
+
+								var _markers = [];
+								$(response).each( function() {
+									if( this.localisation.geolocalisation.geoJson !== undefined ) {
+										var tmp = new google.maps.Marker({
+											position: new google.maps.LatLng(
+												this.localisation.geolocalisation.geoJson.coordinates[1],
+												this.localisation.geolocalisation.geoJson.coordinates[0]
+											),
+											map: map,
+											title: this.nom.libelleFr
+										});
+
+										var communication = '';
+										$(this.informations.moyensCommunication).each( function() {
+											communication += '<abbr title="'+this.type.libelleFr+'">'+this.type.libelleFr+':</abbr> '+this.coordonnee+'<br/>\n';
+										});
+
+									  var infowindow = new google.maps.InfoWindow({
+											content: '<div id="content">\n\
+	<address>\n\
+		<strong>'+this.nom.libelleFr+'</strong><br/>\n\
+		'+(this.localisation.adresse.adresse1!==undefined?this.localisation.adresse.adresse1+'<br/>':'')+
+		(this.localisation.adresse.adresse2!==undefined?this.localisation.adresse.adresse2+'<br/>':'')+
+		(this.localisation.adresse.adresse3!==undefined?this.localisation.adresse.adresse2+'<br/>':'')+'\n\
+		'+this.localisation.adresse.codePostal+' '+this.localisation.adresse.commune.nom+'<br/>\n\
+		'+(communication)+'\n\
+	</address>\n\
+</div>'
+										});
+
+									  google.maps.event.addListener(tmp, 'click', function() {
+									    infowindow.open(map,this);
+									  });
+
+										_markers.push(tmp);
+									}
+								});
+								this._markers = _markers;
+								this._visible = true;
+							}.bind(this)
+						}
+					);
+				}
 			});
 		</script>
 	</body>
